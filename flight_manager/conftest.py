f02a1644c4b2efe202f78ad3d6993509a13386e8
Mustafa Munir <mustafamunir@sastaticket.pk>
2023-01-10 19:26:58 +0500

    Initial

---
+++ flight_manager/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from flight_manager.users.models import User
+from flight_manager.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
