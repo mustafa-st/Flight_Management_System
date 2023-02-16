2484fcc1ebda5f0b54baf184578044b970f7962f
Mustafa Munir <mustafamunir@sastaticket.pk>
2023-02-16 09:54:08 +0500

    feat: create docker services for django admin & postgres

--- flight_manager/celery.py
+++ flight_manager/celery.py
@@ -5,7 +5,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
 
-app = Celery("flight_manager", broker="redis://localhost:6379")
+app = Celery("flight_manager")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.conf.beat_schedule = {
